1fbe6e5f86e217abf4989508991fd59e187e8487
Reshma Remesan <92936449+Reshma41@users.noreply.github.com>
2024-02-29 00:02:06 +0530

    Update dbconnection.js

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/Job")
+mongoose.connect("mongodb+srv://reshmaremesan41:<Reshma@41>@cluster0.hifm2e6.mongodb.net/?Reshma@41=true&w=majority&appName=Cluster0")
 var db=mongoose.connection
 db.on("error",console.error.bind("error"))
 db.once("open",function(){
